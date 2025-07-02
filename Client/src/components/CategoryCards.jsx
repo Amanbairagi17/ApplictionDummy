6a8b6647b61b88dbfd4daaa46069f635034b0d7b
satyam-gurjar <satyagurjar405@gmail.com>
2025-07-02 21:38:48 +0530

    edit card

--- Client/src/components/CategoryCards.jsx
+++ Client/src/components/CategoryCards.jsx
@@ -4,10 +4,7 @@ import Card from './Card'
 const Categorycards = (props) => {
   return (
     <div className='md:flex justify-center  gap-7 '>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
+     
     </div>
   )
 }
