9754ad3739966f09ec4ff548a4ab7e865d3720c3
Tarun007a <tarunchourey16@gmail.com>
2025-08-18 22:13:08 +0530

    code for streak pushed with previous commit and also basic code for oauth google login(not working currently)

--- Backend/src/main/java/com/project/grabtitude/GrabtitudeApplication.java
+++ Backend/src/main/java/com/project/grabtitude/GrabtitudeApplication.java
@@ -7,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class GrabtitudeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GrabtitudeApplication.class, args);
 	}
